1446346a7be925632f297a80f00c6e5714b45637
SpencerKekauoha <spkekauoha@gmail.com>
2017-02-23 21:28:17 -0700

    up to vid 9.  Set up components andbasics of props

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+// Import React from node modules
+import React from 'react';
+//Rather than importing ReactDOM, you can import parts of it
+import { render } from 'react-dom';
+// importing CSS
+import './css/style.css'
+
+//Importing Components
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+render(<App />, document.getElementById('main'))
