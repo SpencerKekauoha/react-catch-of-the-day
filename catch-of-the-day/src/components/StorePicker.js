839f9519df36cdbb33f7d2396de114aea92a4285
SpencerKekauoha <spkekauoha@gmail.com>
2017-02-25 18:12:13 -0700

    imported react router

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // create constructor of Component
+  // constructor() {
+  //   //run super to create a react Component and then creates StorePicker.
+  //   super();
+  //   // Looks for goToStore method and sets itself to itself and binds it to this which inside the constructor, this is equal to the StorePicker Components.
+  //   this.goToStore = this.gotToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('you changed the url');
+    // first grab the text from the box
+
+    //this is not referencing the StorePicker component in ES6 classes.  To access it, create a constructor with super() or onSubmit={ (e) => this.goToStore(e) } or this.goToStore.bind(this)
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    //second we're going to transition from / to /store/:store
+    this.context.router.transitionTo(`/store/${storeId}`);
+
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore.bind(this) }>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={ (input) => {this.storeInput = input} } />
         <button type="Submit">Vist Store →</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,11 +2,27 @@
 import React from 'react';
 //Rather than importing ReactDOM, you can import parts of it
 import { render } from 'react-dom';
+//Import React Router
+import { BrowserRouter, Match, Miss } from 'react-router';
 // importing CSS
 import './css/style.css'
 
 //Importing Components
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App />, document.getElementById('main'))
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        {/* When the url is '/' show the component {StorePicker} */}
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.getElementById('main'));
